b9cca16b43cfd38c18e1d29ea910c99e4c76900b
valentin <vaklap@ethz.ch>
2024-06-05 22:26:45 +0200

    before rehaul

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,33 @@
-var folder = "assets/";
 var space_cover = false;
+
+window.onpopstate = function (event) {
+  console.log("pop: " + event.state);
+  if (event.state == null) {
+    var popUp = $(".komet:not(.inflight)");
+    popUp.animate({ opacity: 0 }, 5000);
+    popUp.css("left", -1 * popUp.width() - 10 + "px");
+    popUp.css("height", [Math.random() * 30 + 3] + "%");
+    popUp.css("left", -1 * popUp.width() - 10 + "px");
+
+    popUp.css("opacity", 1);
+    $(".komet.inflight").each(function () {
+      $(this).removeClass("hidden");
+    });
+    popUp.addClass("inflight");
+  }
+};
 
 function onKometLoad() {
   $("img.komet").on("load", function () {
     var $komet = $(this);
     console.log("i got loaded", $(this));
-    if (space_cover) {
-      $komet.css("opacity", "0");
+    console.log(history.state);
+    if (history.state == null) {
+      $komet.addClass("notransition");
+      $komet.removeClass("hidden");
     }
 
     function animation() {
-      $komet.css("visibility", "visible");
       $komet.css("top", [Math.random() * 79] + "%");
       $komet.css("height", [Math.random() * 30 + 3] + "%");
       $komet.css("left", -1 * $komet.width() - 10 + "px");
@@ -33,25 +50,27 @@ function onKometLoad() {
     // when hovering over image
     $komet.hover(
       function () {
-        if (!space_cover) {
+        console.log(history.state);
+        console.log($(this));
+        if (history.state == null) {
           console.log($(this));
           $(this).css("z-index", "1000");
           $(this).css("animation-play-state", "paused");
         }
       },
       function () {
-        if (!space_cover) {
+        if (history.state == null) {
           $(this).css("animation-play-state", "running");
         }
       },
     );
+
     // when clicking image
     $komet.on("click", function () {
-      if (!space_cover) {
+      if (history.state == null) {
         space_cover = true;
         var offsetLeft = $(this).offset().left;
         $(this).removeClass("inflight");
-        //$(this).css("position", "relative");
         $(this).css("left", offsetLeft + "px");
 
         $(".komet.inflight").each(function () {
@@ -69,24 +88,23 @@ function onKometLoad() {
 
         $(".text").css("visibility", "visible");
         $(".text").addClass("shown");
+        history.pushState("popUp", "", "index.html");
       }
     });
   });
 }
 
+var folder = "assets/";
 $(document).ready(function () {
   $.ajax({
     url: folder,
-    always: function (data) {
+    success: function (data) {
       $(data)
         .find("a")
         .attr("href", function (i, val) {
           if (val.match(/\.(jpg|png|gif)$/)) {
             $(".space").prepend(
-              "<img src='" +
-                folder +
-                val +
-                "' class='komet' style='visibility:hidden'/>",
+              "<img src='" + folder + val + "' class='komet hidden'/>",
             );
           }
         });
